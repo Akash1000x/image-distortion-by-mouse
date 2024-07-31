21be0c8ffdc1b689f8a090f31ef19c25484a5623
Akash kumar kumawat <kumawatap63@gmail.com>
2024-08-01 01:24:41 +0530

    Add data file

---
+++ src/components/data.js
@@ -0,0 +1,30 @@
+export const projects = [
+  {
+    title: "Kashmir",
+    src: "/images/1.jpg",
+  },
+  {
+    title: "Manali",
+    src: "/images/2.jpg",
+  },
+  {
+    title: "Kedarnath",
+    src: "/images/3.jpg",
+  },
+  {
+    title: "Ladakh",
+    src: "/images/4.jpg",
+  },
+  {
+    title: "Rajasthan",
+    src: "/images/5.jpg",
+  },
+  {
+    title: "Goa",
+    src: "/images/6.jpg",
+  },
+  {
+    title: "Kerala",
+    src: "/images/7.jpg",
+  },
+];
