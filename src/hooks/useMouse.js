0e5cecce794f2dc065dfa03b3548e1fa1f09b2f0
Akash kumar kumawat <kumawatap63@gmail.com>
2024-07-31 22:12:32 +0530

    Add useMouse dimension hook

---
+++ src/hooks/useMouse.js
@@ -0,0 +1,31 @@
+import { useMotionValue } from "framer-motion";
+import * as React from "react";
+export default function useMouse() {
+  // const [mouse, setMouse] = useState({x: 0, y: 0});
+
+  // const mouseMove = (e) => {
+  //   const { clientX, clientY } = e;
+  //   setMouse({
+  //       x: clientX,
+  //       y: clientY
+  //   })
+  // }
+  const mouse = {
+    x: useMotionValue(0),
+    y: useMotionValue(0),
+  };
+  const mouseMove = (e) => {
+    const { clientX, clientY } = e;
+    mouse.x.set(clientX);
+    mouse.y.set(clientY);
+  };
+
+  React.useEffect(() => {
+    window.addEventListener("mousemove", mouseMove);
+    return () => {
+      window.removeEventListener("mousemove", mouseMove);
+    };
+  }, []);
+
+  return mouse;
+}
